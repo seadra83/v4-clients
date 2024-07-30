4da67b59fe7c698f0acd89ee862d46554306583a
seadra <156640418+seadra83@users.noreply.github.com>
2024-07-30 18:59:07 +0000

    Remove unused property: account_number

--- v4-client-py/v4_client_py/clients/dydx_subaccount.py
+++ v4-client-py/v4_client_py/clients/dydx_subaccount.py
@@ -1,12 +1,12 @@
-
 from v4_client_py.chain.aerial.wallet import LocalWallet
 from v4_client_py.clients.constants import BECH32_PREFIX
+
 
 class Subaccount:
     def __init__(
         self,
         wallet: LocalWallet,
-        subaccount_number: int=0,
+        subaccount_number: int = 0,
     ):
         self.wallet = wallet
         self.subaccount_number = subaccount_number
@@ -24,9 +24,3 @@ class Subaccount:
     @property
     def address(self) -> str:
         return self.wallet.address().__str__()
-
-    @property
-    def account_number(self) -> int:
-        # Only use account number 0 for now.
-        return 0
-
